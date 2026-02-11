e7014454d902750702c9c9d360fd0ba9b14ba02f
unknown <shanmukakeerthi34@gmail.com>
2026-02-11 10:09:11 +0530

    First CI project

---
+++ OneDrive/Desktop/CI-Lab/test_calculator.py
@@ -0,0 +1,16 @@
+from calculator import add, subtract, multiply, divide
+
+def test_add():
+    assert add(3, 2) == 5
+
+def test_subtract():
+    assert subtract(5, 2) == 3
+
+def test_multiply():
+    assert multiply(4, 2) == 8
+
+def test_divide():
+    assert divide(10, 2) == 5
+
+def test_divide_by_zero():
+    assert divide(5, 0) == "Error"
